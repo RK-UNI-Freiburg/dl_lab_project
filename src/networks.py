d940553850cd85f7fc778250dc09caa8adbfbf4b
Indrashis Das <indrashisdas98@gmail.com>
2023-06-29 18:55:24 +0200

    updated networks

--- src/networks.py
+++ src/networks.py
@@ -21,7 +21,7 @@ class PositionalEncoding(nn.Module):
 class TransformerBlock(nn.Module):
     def __init__(self, input_embedding_size, num_heads, hidden_size, dropout):
         super(TransformerBlock, self).__init__()
-        self.self_attn = nn.MultiheadAttention(embed_dim=input_embedding_size, num_heads=num_heads)
+        self.self_attn = nn.MultiheadAttention(embed_dim=input_embedding_size, num_heads=num_heads, batch_first=True)
         self.norm1 = nn.LayerNorm(input_embedding_size)
         self.linear1 = nn.Linear(in_features=input_embedding_size, out_features=hidden_size)
         self.linear2 = nn.Linear(in_features=hidden_size, out_features=input_embedding_size)
@@ -75,6 +75,7 @@ class EEGTransformer(nn.Module):
             nn.LayerNorm(normalized_shape=input_embedding_size),
             nn.Linear(in_features=input_embedding_size, out_features=num_classes),
         )
+
         for param in self.input_embedding.parameters():
             param.requires_grad = False
 
@@ -88,4 +89,48 @@ class EEGTransformer(nn.Module):
             x = layer(x)
         x = torch.mean(input=x, dim=1)
         x = self.fc(x)
+        return x
+
+
+class EEGTransformerInverse(nn.Module):
+    def __init__(self,
+                 num_layers,
+                 num_channels,
+                 num_heads,
+                 window_size,
+                 input_embedding_size,
+                 hidden_size,
+                 dropout,
+                 num_classes):
+        super(EEGTransformerInverse, self).__init__()
+        self.input_embedding = nn.Linear(in_features=num_channels, out_features=input_embedding_size)
+        self.encoder = nn.ModuleList(
+            [TransformerBlock(input_embedding_size=input_embedding_size,
+                              num_heads=num_heads,
+                              hidden_size=hidden_size,
+                              dropout=dropout) for _ in range(num_layers)])
+        self.fc1 = nn.Linear(in_features=input_embedding_size, out_features=512)
+        self.silu1 = nn.SiLU()
+        self.fc2 = nn.Linear(in_features=512, out_features=256)
+        self.silu2 = nn.SiLU()
+        self.fc3 = nn.Linear(in_features=256, out_features=128)
+        self.silu3 = nn.SiLU()
+        self.fc4 = nn.Linear(in_features=128, out_features=num_classes)
+
+        for param in self.input_embedding.parameters():
+            param.requires_grad = False
+
+    def forward(self, x):
+        x = torch.transpose(x, 1, 2)
+        x = self.input_embedding(x)
+        for layer in self.encoder:
+            x = layer(x)
+        x = x.mean(dim=1)
+        x = self.fc1(x)
+        x = self.silu1(x)
+        x = self.fc2(x)
+        x = self.silu2(x)
+        x = self.fc3(x)
+        x = self.silu3(x)
+        x = self.fc4(x)
         return x
